089722d2624e6f2f1177e3fbdd54f51e65324893
Robert Pearce <me@robertwpearce.com>
2020-04-26 11:21:01 -0400

    first commit

---
+++ release.nix
@@ -0,0 +1,52 @@
+{ compiler ? "ghc883"
+, sources ? import ./nix/sources.nix
+}:
+
+let
+  overlay = _: pkgz: {
+    niv = import sources.niv { };
+  };
+
+  pkgs = import sources.nixpkgs {
+    config = {
+      packageOverrides = pkgz: rec {
+        haskell = pkgz.haskell // {
+          packages = pkgz.haskell.packages // {
+            ${compiler} = pkgz.haskell.packages.${compiler}.override {
+              overrides = hpNew: hpOld: rec {
+                hakyll = hpOld.hakyll.overrideAttrs(oldAttrs: {
+                  configureFlags = "-f watchServer -f previewServer";
+                  patches = [ ./hakyll.patch ];
+                });
+
+                project = hpNew.callCabal2nix "hakyll-nix-example" ./. { };
+              };
+            };
+          };
+        };
+      };
+    };
+
+    overlays = [ overlay ];
+  };
+
+  haskellPackages = pkgs.haskell.packages.${compiler};
+in
+  {
+    project = haskellPackages.project;
+
+    shell = haskellPackages.shellFor {
+      packages = p: with p; [
+        haskellPackages.project
+      ];
+      buildInputs = with haskellPackages; [
+        ghcid
+        hlint
+        niv
+        #ormolu
+        pkgs.cacert
+        pkgs.nix
+      ];
+      withHoogle = true;
+    };
+  }
